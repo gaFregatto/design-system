39016ed7d5fcdc1d75a6980cc09d4168c5b9c681
Guilherme Arai Fregatto <guilherme_arai19@hotmail.com>
2022-11-09 16:59:02 -0300

    added documentation for some of the components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typograph/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero veritatis molestiae quos, eum consequuntur ratione saepe et odio asperiores neque fugiat officia, unde praesentium error voluptatibus ea in iure aut.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
